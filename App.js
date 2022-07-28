38d0bc96c8bd1942337b20f02e3083af1ce1fa37
Hardik Savani <itsolutionstuff@gmail.com>
2022-07-28 10:54:15 +0530

    get ip address

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { expo } from './app.json';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
 
 export default function App() {
+
+  const [ip, setIP] = useState('');
+
+  /*------------------------------------------
+  --------------------------------------------
+  Getting IP using API
+  --------------------------------------------
+  --------------------------------------------*/
+  const getData = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/')
+    setIP(res.data.IPv4)
+  }
+  
+  /*------------------------------------------
+  --------------------------------------------
+  Call getData() function
+  --------------------------------------------
+  --------------------------------------------*/
+  useEffect( () => {
+    getData()
+
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Your App Version is {expo.version}</Text>
+      <Text>Your IP Address is {ip}</Text>
       <StatusBar style="auto" />
     </View>
   );
